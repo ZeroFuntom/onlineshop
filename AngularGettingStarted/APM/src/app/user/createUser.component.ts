fd01f2cf2e7f41e5ac30cfd1c12ad87c5136e4dd
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2017-09-22 16:21:44 +0200

    +added stuff

--- AngularGettingStarted/APM/src/app/user/createUser.component.ts
+++ AngularGettingStarted/APM/src/app/user/createUser.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
     templateUrl: './createUser.component.html',
     styleUrls: ['./createUser.component.css']
 })
+
 export class CreateUserComponent {
+        public pageTitle: string = 'Create a new User-Profile';
 }
