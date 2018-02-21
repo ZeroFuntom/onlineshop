472b6453a721483288d5599c0e73dbd08fa7f870
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2018-02-21 09:09:46 +0100

    New Files added Interfaces and Services added WEB.API made

--- AngularGettingStarted/APM-Final/src/app/user/createUser.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './createUser.component.html'
-})
-export class WelcomeComponent {
-    public pageTitle: string = 'Welcome';
-}
