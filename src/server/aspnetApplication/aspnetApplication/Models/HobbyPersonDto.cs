a4dcef490a90316dda1129a4bca79ece557200c6
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2018-03-02 09:29:05 +0100

    +File

---
+++ src/server/aspnetApplication/aspnetApplication/Models/HobbyPersonDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspnetApplication.Models
+{
+    public class HobbyPersonDto
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public int HobbyId { get; set; }
+        public string HobbyName { get; set; }
+    }
+}
