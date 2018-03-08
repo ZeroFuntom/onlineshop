5c69d348bb1454d0ad29d7a40e5cbef3e75fb995
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2018-03-08 15:31:11 +0100

    +

---
+++ src/client/src/app/create-new/create-new.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { AppModule } from '../app.module';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+    templateUrl: './create-new.component.html',
+    styles: [`./create-new.component.css`],
+})
+
+export class CreateNewComponent {
+
+}
